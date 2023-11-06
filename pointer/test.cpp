39010f1baddd9970d92126536ff69d190ab1a9e2
AbrarZahid050 <abrar.zahid050@gmail.com>
2023-11-06 19:22:15 +0500

    testing copy-constructor.

--- pointer/test.cpp
+++ pointer/test.cpp
@@ -1,31 +1,72 @@
 #include <iostream>
-#include <string>
 
 class ptrMemberVarType {
 public:
-    ptrMemberVarType(); // Constructor
+    void print() const;
+    void insertAt(int index, int num);
+    ptrMemberVarType(int size = 10); // Constructor
+    ptrMemberVarType(const ptrMemberVarType& otherObject); // copy-constructor
     ~ptrMemberVarType(); // Destructor
 
 private:
-    int x;
-    int lenP;
+    int maxSize;
+    int length;
     int* p;
 };
 
 using namespace std;
 
 int main() {
-    ptrMemberVarType objectOne;
-    ptrMemberVarType objectTwo;
-
-    cout << "=> [main]" << endl;
+    cout << "=> [main]\n";
     return 0;
 }
 
-ptrMemberVarType::ptrMemberVarType() {
-    cout << "-> [Constructor] " << endl;
+ptrMemberVarType::ptrMemberVarType(int size) {
+    cout << "-> [Constructor]\n";
+    if (size <= 0) {
+        cout << "<- [Constructor] array size must be greater than 0,\n";
+        cout << "    creating an array of size 10.\n";
+        maxSize = 10;
+    }
+    else {
+        maxSize = size;
+    }
+    length = 0;
+    p = new int[maxSize];
 }
 
 ptrMemberVarType::~ptrMemberVarType() {
-    cout << "-> [Destructor] clean-up." << endl;
+    cout << "-> [Destructor] clean-up.\n";
+    delete[] p; // deallocating the memory of dynamic array.
 }
+
+ptrMemberVarType::ptrMemberVarType(const ptrMemberVarType& otherObject) {
+    cout << "-> [Copy-constructor]\n";
+    maxSize = otherObject.maxSize;
+    length = otherObject.length;
+    p = new int[maxSize];
+
+    for (int i = 0; i < length; i++) {
+        p[i] = otherObject.p[i];
+    }
+}
+
+void ptrMemberVarType::print() const {
+    cout << "-> [print]\n";
+    for (int i = 0; i < length; i++) {
+        cout << "   p[" << i << "] - " << p[i] << endl;
+    }
+}
+
+void ptrMemberVarType::insertAt(int index, int num) {
+    assert(index >= 0 && index < maxSize);
+
+    if (index < length) {
+        p[index] = num;
+    }
+    else {
+        p[length] = num;
+        length++;
+    }
+}
+
