efe37a9364acee03f22ee7cc35fb2e1086b4c5dd
AbrarZahid050 <abrar.zahid050@gmail.com>
2023-11-10 00:30:41 +0500

    virtual function done example-1

--- inheritance-pointer-virtualFunction/example1.cpp
+++ inheritance-pointer-virtualFunction/example1.cpp
@@ -1,59 +1,31 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
 
 class petType {
 public:
-    void print() const;
-    petType(string = "");
-
-private:
-    string name;
+    virtual void print();
 };
 
 class dogType : public petType {
 public:
-    void print() const;
-    dogType(string = "", string = "");
-
-private:
-    string breed;
+    void print();
 };
 
-petType::petType(string n) {
-    cout << "-> [petType] constructor.\n";
-    name = n;
+void petType::print() {
+    std::cout << "-> [petType]\n";
 }
 
-void petType::print() const {
-    cout << "-> [petType] print: " << name << endl;
-}
-
-dogType::dogType(string n, string b) : petType(n) {
-    cout << "-> [dogType] constructor.\n";
-    breed = b;
-}
-
-void dogType::print() const {
-    petType::print();
-    cout << "-> [dogType] print: " << breed << endl;
-}
-
-void callPrint(petType& p);
-
-int main() {
-    petType pet("Lucky");
-    dogType dog("Tommy", "German Shepherd");
-
-    pet.print();
-    dog.print();
-
-    cout << "*** Calling callPrint function ***\n";
-    callPrint(pet);
-    return 0;
+void dogType::print() {
+    std::cout << "-> [dogType]\n";
 }
 
 void callPrint(petType& p) {
     p.print();
+}
+
+int main() {
+    petType pet;
+    dogType dog;
+
+    std::cout << "*** Calling callPrint ***\n";
+    callPrint(dog);
 }
